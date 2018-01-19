fdbb5a0d125a1d5f3d47808b8fdc0f4c9e38c479
mightytomsen <w.wuller@gmx.de>
2018-01-19 14:48:29 +0100

    Update Header.tpl.php

--- www/template/header/Header.tpl.php
+++ www/template/header/Header.tpl.php
@@ -2,19 +2,19 @@
 <head>
     <title><?php echo $this->controllerConfig()["title"]; ?></title>
 
-    <link rel="stylesheet" href="/hubbafever/www/public/css/main.css">
-    <link rel="stylesheet" href="/hubbafever/www/public/css/font-awesome.css">
+    <link rel="stylesheet" href="/MVC/www/public/css/main.css">
+    <link rel="stylesheet" href="/MVC/www/public/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="images/lol_icon.jpg">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
     <meta name="description"
-          content="ELO Boost Service und Coaching für League of Legends (LoL). Verfügbar auf jedem Server!"/>
+          content=""/>
     <meta name="keywords"
-          content="elo boost, elo boosting, lol boost, lol elo, elo, boosting, boost, league of legends, lol, cheap elo, league points, lol tier, lol division, league boosting, mmr boosting"/>
-    <meta property="og:site_name" content="fast-boost.net">
-    <meta property="og:description" content="Elo Boosting - League of Legends">
-    <meta property="og:url" content="https://fast-boost.net">
+          content="-"/>
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="-">
     <meta property="og:title" content="Home">
 </head>
